fefe18b973821e00d5c4948fe2db838dc7e439b6
Aymeric <aymeric.lamboley@gmail.com>
2015-01-30 12:20:08 +0100

    sounds like we have some issues with android lib path...

--- android/src/com/davikingcode/nativeExtensions/googlePlus/LoginActivity.java
+++ android/src/com/davikingcode/nativeExtensions/googlePlus/LoginActivity.java
@@ -4,15 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.davikingcode.nativeExtensions.googlePlus.GooglePlusExtension;
-import com.davikingcode.nativeExtensions.googlePlus.GooglePlusExtensionContext;
+//import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
 
-import java.lang.Override;
-import java.lang.String;
-
+//public class LoginActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 public class LoginActivity extends Activity {
 
     private GooglePlusExtensionContext _context = null;
+    
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,37 @@ public class LoginActivity extends Activity {
 
         Log.d("GooglePlusANE", String.valueOf(_context.getResourceId("layout.sign_in_activity")));
 
-        setContentView(_context.getResourceId("layout.sign_in_activity"));
+        //setContentView(_context.getResourceId("layout.sign_in_activity"));
+        
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(Plus.API).build();
+        //mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(Plus.API).addConnectionCallbacks(this).addOnConnectionFailedListener(this).build();
+        
+        Log.d("GooglePlusANE", "mGoogleApiClient created");
+        
+        mGoogleApiClient.connect();
+        
+        Log.d("GooglePlusANE", "mGoogleApiClient connect");
     }
+    
+    /*@Override
+    public void onConnected(Bundle connectionHint) {
+    	
+    	Log.d("GooglePlusANE", "Connected!!!!!");
+    	
+    	Person person = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+    	
+    	Log.d("GooglePlusANE", "Perso: " + person.getDisplayName());
+    }
+    
+    @Override
+    public void onConnectionSuspended(int cause) {
+    	
+    	Log.d("GooglePlusANE", "onConnectionSuspended!!!!!");
+    }
+    
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+    	
+    	Log.d("GooglePlusANE", "onConnectionFailed!!!!!");
+    }*/
 }
